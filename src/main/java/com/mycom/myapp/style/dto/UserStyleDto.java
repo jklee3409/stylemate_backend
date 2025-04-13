dd6854c652711ccf835da64f3d238ae859279287
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-04-13 10:50:36 +0900

    feat(dto) : UserStyleDto 구현

---
+++ src/main/java/com/mycom/myapp/style/dto/UserStyleDto.java
@@ -0,0 +1,16 @@
+package com.mycom.myapp.style.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStyleDto {
+
+    private long userId;
+    private int styleId;
+}
