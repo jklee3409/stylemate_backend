71ee0f5a94783fde00662984db873e7ad350237a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-04-13 10:50:28 +0900

    feat(dto) : StyleDto 구현

---
+++ src/main/java/com/mycom/myapp/style/dto/StyleDto.java
@@ -0,0 +1,16 @@
+package com.mycom.myapp.style.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StyleDto {
+
+    private int styleId;
+    private String styleName;
+}
